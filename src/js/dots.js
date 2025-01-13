3d3af41d792d3bac456805fc22f6eb346f8cf746
progikusok <moyomylo11@gmail.com>
2025-01-13 10:07:33 +0300

    feat: add cursor, dots, gsap

---
+++ src/js/dots.js
@@ -0,0 +1,100 @@
+export class DotGrid {
+  constructor(container = "sketch") {
+    this.canvasElement = document.getElementById(container);
+
+    this.measureDimensions();
+
+    this.mouseX = 0;
+    this.mouseY = 0;
+  }
+
+  measureDimensions() {
+    this.dpr = window.devicePixelRatio || 1;
+
+    this.drawable = this.canvasElement.getBoundingClientRect();
+
+    this.canvasWidth = this.drawable.width * this.dpr;
+    this.canvasHeight = this.drawable.height * this.dpr;
+
+    this.canvasElement.width = this.canvasWidth;
+    this.canvasElement.height = this.canvasHeight;
+
+    this.canvas = this.canvasElement.getContext("2d");
+    this.canvas.scale(this.dpr, this.dpr);
+  }
+
+  onWindowResize() {
+    this.measureDimensions();
+
+    this.draw();
+  }
+
+  onMouseUpdate(e) {
+    this.mouseX = e.pageX - this.drawable.left;
+    this.mouseY = e.pageY - this.drawable.top;
+    this.drawable = this.canvasElement.getBoundingClientRect();
+
+    window.requestAnimationFrame(this.draw.bind(this));
+  }
+
+  init() {
+    window.requestAnimationFrame(this.draw.bind(this));
+    // Listen for Mouse updates
+    document.body.addEventListener(
+      "mousemove",
+      this.onMouseUpdate.bind(this),
+      false
+    );
+
+    window.addEventListener("resize", this.onWindowResize.bind(this));
+  }
+
+  // Draws the background and calls the function for drawing the dots
+  draw() {
+    this.canvas.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    this.drawDots();
+  }
+
+  /*
+  ((j - this.mouseY) / dist * 4)
+  */
+
+  // i and j function as x and y when drawing the dot grid.
+  drawDots() {
+    let size = 1.2;
+    let gridSize = 30;
+    for (var i = 2; i < this.canvasWidth / this.dpr / gridSize - 1; i++) {
+      for (var j = 2; j < this.canvasHeight / this.dpr / gridSize - 1; j++) {
+        let x = i * gridSize;
+        let y = j * gridSize;
+        let dist = this.calculateDistance(x, y, this.mouseX, this.mouseY);
+        this.canvas.beginPath();
+        this.canvas.arc(
+          x + ((x - this.mouseX) / dist) * gridSize,
+          y + ((y - this.mouseY) / dist) * gridSize,
+          size,
+          size,
+          Math.PI,
+          true
+        );
+        this.canvas.fillStyle = "black";
+        this.canvas.fill();
+      }
+    }
+  }
+
+  // Grabs mouse position, checks if the mouse is off the screen (NaN) and calculates the distance from the mouse pointer and each dot using the pythagorean theorem.
+  calculateDistance(ellipseX, ellipseY, mouseX, mouseY) {
+    let x = mouseX;
+    let y = mouseY;
+
+    if (x == NaN) {
+      return 1;
+    } else {
+      let leg1 = Math.abs(x - ellipseX);
+      let leg2 = Math.abs(y - ellipseY);
+      let pyth = Math.pow(leg1, 1.8) + Math.pow(leg2, 1.8);
+      return Math.sqrt(pyth);
+    }
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,88 @@
 import "../scss/style.scss";
+import { DotGrid } from "./dots.js";
+
+// produceCelledBlockLogic();
+
+// window.addEventListener("resize", produceCelledBlockLogic);
+
+function produceCelledBlockLogic() {
+  const celledBlock = document.getElementsByClassName("celled-block")[0];
+
+  celledBlock.innerHTML = "";
+
+  const containerDimensions = {
+    w: celledBlock.clientWidth,
+    h: celledBlock.clientHeight,
+  };
+
+  const numInRow = 15;
+
+  const cellSize = containerDimensions.w / numInRow;
+
+  const numInColumn = Math.floor(containerDimensions.h / cellSize);
+
+  celledBlock.style.setProperty("--cell-size", cellSize);
+  celledBlock.style.setProperty("--columns", numInRow);
+
+  const spans = Array.from({ length: numInRow * numInColumn }).map(() => {
+    const span = document.createElement("span");
+    span.setAttribute("class", "celled-block__item");
+
+    return span;
+  });
+
+  celledBlock.append(...spans);
+
+  console.log(celledBlock.clientWidth, containerDimensions, spans);
+}
+
+import { GooCursor } from "./cursor.js";
+
+const cursorEl = document.querySelector(".cursor");
+
+// Initialize cursor
+// const goo = new GooCursor(cursorEl);
+
+// doMagic();
+
+// Easter egg: click anywhere
+
+function doMagic() {
+  gsap
+    .timeline()
+    .addLabel("start", 0)
+    .to(
+      goo.DOM.cells,
+      {
+        duration: 1,
+        ease: "power4",
+        opacity: 1,
+        stagger: {
+          from: [...goo.DOM.cells].indexOf(goo.getCellAtCursor()),
+          each: 0.02,
+          grid: [goo.rows, goo.columns],
+        },
+      },
+      "start"
+    )
+    .to(
+      goo.DOM.cells,
+      {
+        duration: 1,
+        ease: "power1",
+        opacity: 0,
+        stagger: {
+          from: [...goo.DOM.cells].indexOf(goo.getCellAtCursor()),
+          each: 0.03,
+          grid: [goo.rows, goo.columns],
+        },
+      },
+      "start+=0.3"
+    );
+}
+
+window.addEventListener("click", () => {
+  doMagic();
+});
+
+new DotGrid("sketch").init();
